caf25fd2928e803cdc393ba764cb452ca5d06f5e
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-12-07 18:16:43 -0500

    Observation operator formulation

--- howland-ts-nimble.R
+++ howland-ts-nimble.R
@@ -17,47 +17,47 @@ dat <- read_csv("data/sites/site_US-Ho1/Ho1-flux.csv") %>%
 # Nimble
 gpp_code <- nimbleCode({
   # Process model -- random walk
-  gpp[1] ~ dnorm(0, 0.1)
+  gpp[1] ~ T(dnorm(0, 0.1), 0, Inf)
+  r[1] ~ T(dnorm(0, 0.1), 0, Inf)
   for (t in 2:nT) {
-    gpp[t] ~ dnorm(gpp[t-1], tau_rw)
+    gpp[t] ~ T(dnorm(gpp[t-1], tau_rw), 0, Inf)
+    r[t] ~ T(dnorm(r[t-1], tau_rw), 0, Inf)
   }
+  nep[1:nT] <- gpp[1:nT] - r[1:nT]
+  rb_term <- 1 + Rho / Beta - Rho
+  rsoil[1:nT] <- r[1:nT] / rb_term
   # Data model
   for (t in 1:nT) {
-    r[t] <- r_chamber[t] * (1 + Rho / Beta - Rho)
-    nep_pred[t] <- gpp_finality[t] - r[t]
-    nep[t] ~ dnorm(nep_pred[t], tau_nep)
-    gpp_finality[t] ~ dnorm(gpp[t], tau_finality)
+    rsoil_obs[t] ~ dnorm(rsoil[t], tau_rsoil)
+    nep_obs[t] ~ dnorm(nep[t], tau_nep)
   }
   # Priors
   tau_rw ~ dgamma(0.1, 0.1)
-  ## tau_finality ~ dgamma(10, 10)
-  ## Rho ~ dnorm(rho_mu, sd = rho_sd)
-  ## Beta ~ dnorm(Beta_mu, sd = Beta_sd)
   Rho ~ dbeta(rho_a, rho_b)
   Beta ~ dnorm(Beta_a, Beta_b)
 })
 gpp_data <- list(
-  r_chamber = dat$Rsoil,
-  nep = dat$NEE,
+  rsoil_obs = dat$Rsoil,
+  nep_obs = dat$NEE,
   ## rho_mu = 0.56, rho_sd = 0.11,
   ## Beta_mu = 0.5, Beta_sd = 0.125,
   rho_a = 15, rho_b = 15,
   Beta_a = 15, Beta_b = 15,
-  tau_nep = 1000, tau_finality = 1000
+  tau_nep = 1000,
+  tau_rsoil = 1000
 )
 samples <- nimbleMCMC(
   code = gpp_code,
   data = gpp_data,
   constants = list(nT = nrow(dat)),
-  monitors = c("gpp", "gpp_finality", "nep_pred",
-               "Rho", "Beta",
-               "tau_rw")
+  monitors = c("gpp", "nep", "rsoil",
+               "Rho", "Beta", "tau_rw")
 )
 
-samples_gpp <- samples[,grep("gpp\\[", colnames(samples))]
-samples_npp <- samples[,grep("nep_pred", colnames(samples))]
+samples_gpp <- samples[, grep("gpp\\[", colnames(samples))]
+samples_nep <- samples[, grep("nep\\[", colnames(samples))]
 gpp_mean <- colMeans(samples_gpp)
-nep_mean <- colMeans(samples_npp)
+nep_mean <- colMeans(samples_nep)
 gpp_quant <- apply(samples_gpp, 2, quantile, c(0.025, 0.975))
 dat2 <- dat %>%
   mutate(
